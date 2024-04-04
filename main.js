afab794e6edd3a916a5756e8b20efdf0ccb0b8bf
Razane Abou Karroum <razaneaboukarroum@hotmail.com>
2024-04-04 16:18:35 +0300

    added some styling to the contact file

---
+++ main.js
@@ -0,0 +1,21 @@
+//document.addEventListener("click", closeToggler);
+
+// function closeToggler() {
+//   //   let checkbox = document.querySelector("#check");
+//   //   if (checkbox.checked === false) {
+//   //     console.log("checkbox unchecked");
+//   //     document.querySelector("#check").checked = false;
+//   //   }
+//   document.getElementById("check").checked = false;
+// }
+
+// const menu = document.querySelector(".navbar");
+// const menuBtn = document.querySelector("#check");
+
+// document.addEventListener("click", (e) => {
+//   if (!menu.contains(e.target)) {
+//     menuBtn.checked = false;
+//   }
+// });
+
+// && e.target !== menuBtn
